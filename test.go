16ac4494c63c976b5f4d60d2db35e3c818dec627
Yulia Nosova <yuliya-nosova1996@yandex.ru>
2018-09-12 13:00:02 +0300

    Hello_World

--- test.go
+++ test.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello woooooorld")
+	http.HandleFunc("/", sayhello)           // Устанавливаем роутер
+	err := http.ListenAndServe(":8080", nil) // устанавливаем порт веб-сервера
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+}
+func sayhello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello woooooorld")
 }
