0d248d76640d9e3b2a01697456adde291eb1a409
Yuliya Nosova <yuliya-nosova1996@yandex.ru>
2018-10-09 13:53:30 +0300

    Client-Server на старом примере

--- Client/Client_window.go
+++ Client/Client_window.go
@@ -5,9 +5,33 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
 	"github.com/andlabs/ui"
 	_ "github.com/andlabs/ui/winmanifest"
 )
+
+func sendMessage(message string) string {
+	client := http.Client{}
+	resp, err := client.Get("http://localhost:8080/" + message)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err2 := ioutil.ReadAll(resp.Body)
+		if err2 != nil {
+			fmt.Println(err)
+			return ""
+		}
+		bodyString := string(bodyBytes)
+		return bodyString
+	}
+	return ""
+}
 
 var mainwin *ui.Window
 
@@ -18,9 +42,6 @@ func Client() ui.Control {
 	vbox := ui.NewVerticalBox()
 	vbox.SetPadded(true)
 	hbox.Append(vbox, false)
-
-	hbox.Append(ui.NewVerticalSeparator(), false)
-
 	vbox = ui.NewVerticalBox()
 	vbox.SetPadded(true)
 	hbox.Append(vbox, true)
@@ -28,26 +49,6 @@ func Client() ui.Control {
 	grid := ui.NewGrid()
 	grid.SetPadded(true)
 	vbox.Append(grid, false)
-
-	button := ui.NewButton("Open File")
-	entry := ui.NewEntry()
-	entry.SetReadOnly(true)
-	button.OnClicked(func(*ui.Button) {
-		filename := ui.OpenFile(mainwin)
-		if filename == "" {
-			filename = "(cancelled)"
-		}
-		entry.SetText(filename)
-	})
-	grid.Append(button,
-		0, 0, 1, 1,
-		false, ui.AlignFill, false, ui.AlignFill)
-
-	msggrid := ui.NewGrid()
-	msggrid.SetPadded(true)
-	grid.Append(msggrid,
-		0, 2, 2, 1,
-		false, ui.AlignCenter, false, ui.AlignStart)
 
 	//поле ввода текста
 	group := ui.NewGroup("")
@@ -61,6 +62,23 @@ func Client() ui.Control {
 	group.SetChild(entryForm)
 
 	entryForm.Append("Enter text", ui.NewEntry(), false)
+
+	button := ui.NewButton("Connect")
+	entry := ui.NewEntry()
+	entry.SetReadOnly(true)
+	button.OnClicked(func(*ui.Button) {
+		ui.MsgBox(mainwin, sendMessage("Hello"), sendMessage("I am server!"))
+	})
+	grid.Append(button,
+		0, 0, 1, 1,
+		false, ui.AlignFill, false, ui.AlignFill)
+
+	msggrid := ui.NewGrid()
+	msggrid.SetPadded(true)
+	grid.Append(msggrid,
+		0, 2, 2, 1,
+		false, ui.AlignCenter, false, ui.AlignStart)
+
 	return hbox
 
 }
